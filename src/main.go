1b6964451e282758664be33a755f347fa9dcc1df
OTakumi <takumi.o.0410@gmail.com>
2021-11-01 23:47:28 +0900

    Add html template feature. There is a bug that js files cannot be loaded.

--- src/main.go
+++ src/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHttp method processes HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("../view/html/",
+				t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	// root
+	dir, _ := os.Getwd()
+	log.Print(http.Dir(dir + "../view/dist/js/"))
+	http.Handle("/", &templateHandler{filename: "index.html"})
+	// http.Handle("../view/dist/js/", http.StripPrefix("../view/dist/js/",
+	// 	http.FileServer(http.Dir(dir+"../view/dist/js/"))))
+
+	// Start the server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
